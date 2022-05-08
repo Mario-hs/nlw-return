42c6f5c63c61613efdd47a830a9b36386685b5a2
Mario Henrique <mario-hp41@hotmail.com>
2022-05-08 20:05:06 -0300

    mobile

---
+++ mobile/src/components/Success/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Image, TouchableOpacity, Text } from "react-native";
+
+import successImg from "../../assets/success.png";
+
+import { Copyright } from "../Copyright";
+
+import { styles } from "./styles";
+
+interface Props {
+    onSendAnotherFeedbacks: () => void;
+}
+
+export function Success({ onSendAnotherFeedbacks }: Props) {
+    return (
+        <View style={styles.container}>
+
+            <Image source={successImg} style={styles.image} />
+            <Text style={styles.title}>Agradecemos o seu feedback</Text>
+
+            <TouchableOpacity style={styles.button} onPress={onSendAnotherFeedbacks}>
+
+                <Text style={styles.buttonTitle}>
+                    Quero enviar outro
+                </Text>
+
+            </TouchableOpacity>
+
+            <Copyright />
+
+        </View>
+    )
+}
